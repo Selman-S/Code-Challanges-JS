2f2c9e1c37cd7a31421b15cae0c26cd6ff6273e0
selman <gunduzgece546@gmail.com>
2022-06-12 18:44:35 +0300

    missing letter

--- challanges.js
+++ challanges.js
@@ -30,9 +30,85 @@ function sum(a){
 
 
 
- 
+
+//   2 sor 
+
+
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// samples:
+// isValidWalk(['n','s','n','s','n','s','n','s','n','s']) => true
+// isValidWalk(['w']) => false
+// isValidWalk(['n','n','n','s','n','s','n','s','n','s']) => false
+// isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']) => false
+
+
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']));
+console.log(isValidWalk(['w']));
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']));
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']));
 
   
+
+function isValidWalk(yön){
+  if(yön.length !=10)return  false;
+  let dir = {'w':0, 'e':0,'s':0,'n':0};
+  
+  for (let i = 0; i < yön.length; i++) {
+    dir[yön[i]]+=1;
+  }
+  if (dir['w']==dir['e'] && dir['s']==dir['n']) {
+        return true
+      }else{
+        return false
+      }
+} 
+
+
+
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Samples:
+// ['a','b','c','d','f'] -> 'e' 
+// ['O','Q','R','S'] -> 'P'
+
+
+
+
+console.log(findMissingLetter(['O','Q','R','S']));
+console.log(findMissingLetter(['a','b','c','d','f']));
+function findMissingLetter(x)
+{
+  const lenX = x.length;
+  let letter =x.map((i)=>i.charCodeAt())
+  let ilk = letter[0];
+  for (let i = letter[0]; i < letter[letter.length-1]; i++) {
+    if (!letter.includes(i)) {
+      return String.fromCharCode(i);
+    }
+  } 
+  return ' ';
+}
+    
+
+
+
+    
+    
+    
  
+  
+  
+
+
+
+
+
+
+
 
 
